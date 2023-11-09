4002bffd5fe93850793694156074203724ec5c8f
Martin-kod <>
2023-11-09 22:14:14 +0000

    Add Booking model class in models.py

--- thehut/models.py
+++ thehut/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Booking(models.Model):
+    name = models.CharField(max_length=60)
+    phone = models.IntegerField()
+    email = models.EmailField(max_length=30)
+    num_guests = models.IntegerField(choices=[(1, 1), (2, 2)])
+    date = models.DateField()
+    time = models.CharField(max_length=1, choices=[
+        (1, '16:00'), (2, '18:00'), (3, '20:00')])
+
+    class Meta:
+        ordering = ['date']
